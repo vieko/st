ca9a24941b3052f9aaed19f23edbb1a76e7836c9
Vieko Franetovic <vieko.franetovic@gmail.com>
2018-09-21 14:56:30 -0600

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=30:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=28:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
